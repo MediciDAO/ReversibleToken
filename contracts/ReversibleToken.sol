6d18bfa939b95f5fe87c0a7dce57418789d67d96
decanus <dean.eigenmann@icloud.com>
2018-04-29 16:18:02 +0200

    finished

--- contracts/ReversibleToken.sol
+++ contracts/ReversibleToken.sol
@@ -17,12 +17,14 @@ contract ReversibleToken is ERC20 {
 
     ERC20 public token;
 
+    uint256 totalSupply_;
     uint public escrow;
 
     uint constant public REVERSIBLE_TIMEFRAME = 30 days;
 
     Transaction[] public transfers;
     mapping (address => uint) private balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
 
     event Reversed(uint transaction); // @todo more info
     event Confirmed(uint transaction);
@@ -34,10 +36,12 @@ contract ReversibleToken is ERC20 {
     function deposit(uint amount) external {
         require(token.transferFrom(msg.sender, address(this), amount));
         balances[msg.sender] = balances[msg.sender].add(amount);
+        totalSupply_.add(amount);
     }
 
     function withdraw(uint amount) external {
         balances[msg.sender] = balances[msg.sender].sub(amount);
+        totalSupply_.sub(amount);
         require(token.transfer(msg.sender, amount));
     }
 
@@ -64,6 +68,31 @@ contract ReversibleToken is ERC20 {
         escrow = escrow.sub(transfer.amount);
         balances[transfer.to] = balances[transfer.to].add(transfer.amount);
         emit Confirmed(transaction);
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(to != address(0));
+        require(value <= balances[from]);
+        require(value <= allowed[from][msg.sender]);
+        transfer(from, to, value);
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        transfer(msg.sender, to, value);
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    function totalSupply() public view returns (uint) {
+        return totalSupply_;
     }
 
     function transfer(address from, address to, uint amount) internal {
